b4663fe4650814b30b6c87e9958c3685fbaecc03
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-08-11 17:37:15 +1000

    fluorine: Suspend on low battery

--- nixos/per-device/fluorine.nix
+++ nixos/per-device/fluorine.nix
@@ -22,6 +22,15 @@
       fsType = "ext4";
     };
 
+  # Suspend on low battery
+  services.udev.extraRules = builtins.concatStringsSep ", " [
+    ''SUBSYSTEM=="power_supply"''
+    ''ATTR{status}=="Discharging"''
+    ''ATTR{capacity}=="[0-9]"''
+    ''RUN+="${pkgs.systemd}/bin/systemctl suspend"
+    ''
+  ];
+
   # Enable hardware video rendering
   hardware.opengl.extraPackages = [ pkgs.vaapiIntel ];
 
