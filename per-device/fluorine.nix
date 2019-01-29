461e2bddf28cd4a323f1386d893af9347f08cf7f
Matthew Harm Bekkema <mbekkema97@gmail.com>
2019-01-29 14:31:25 +1100

    fluorine: Move partition config out

--- per-device/fluorine.nix
+++ per-device/fluorine.nix
@@ -4,23 +4,7 @@
   networking.hostName = "fluorine";
 
   # Full-disk encryption + boot from external USB
-  boot.initrd.luks.devices.sda1_crypt.device = "/dev/sda1";
   boot.loader.grub.device = "nodev";
-
-  fileSystems."/home/matthew/owncloud" =
-    { device = "/dev/vgfluorine/owncloud";
-      fsType = "ext4";
-    };
-
-  fileSystems."/media/determination" =
-    { device = "/dev/vgfluorine/determination";
-      fsType = "ext4";
-    };
-
-  fileSystems."/media/stretch" =
-    { device = "/dev/vgfluorine/jessie";
-      fsType = "ext4";
-    };
 
   # Suspend on low battery
   services.udev.extraRules = builtins.concatStringsSep ", " [
