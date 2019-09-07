b644870469cdc77d5acde6038541e90ce102bc7b
Matthew Harm Bekkema <mbekkema97@gmail.com>
2019-09-08 01:00:17 +1000

    gallium: Narrow down necessary kernel config

--- per-device/gallium.nix
+++ per-device/gallium.nix
@@ -17,26 +17,14 @@
   # Need this kernel stuff for new AMD APU
   boot.kernelPackages = pkgs.linuxPackages_4_19;
   boot.initrd.kernelModules = [ "amdgpu" ];
-  boot.kernelParams = [ "rcu_nocbs=0-15" ];
 
   boot.kernelPatches = [{
     name = "my-config";
     patch = null;
     extraConfig = ''
       X86_AMD_PLATFORM_DEVICE y
-      DRM_AMD_DC_DCN1_0 y
       PREEMPT_VOLUNTARY n
       PREEMPT y
-      PREEMPT_RCU y
-      RCU_EXPERT y
-      RCU_STALL_COMMON y
-      RCU_NEED_SEGCBLIST y
-      RCU_FANOUT 32
-      RCU_FANOUT_LEAF 16
-      RCU_BOOST y
-      RCU_BOOST_DELAY 300
-      RCU_NOCB_CPU y
-      RCU_CPU_STALL_TIMEOUT 60
     '';
   }];
 
